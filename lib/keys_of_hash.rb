b3e925acded1b4fc807cab1fbfdcea478b1da350
MaryJay787 <josephmaryam.mj@gmail.com>
2019-05-28 10:55:37 -0400

    Keys of Done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+  b = []
+
+  self.map do |hash_key, v|
+    arguments.map do |key|
+      if v == key
+        b << hash_key
+        end
+      end
+    end
+    b
   end
 end
